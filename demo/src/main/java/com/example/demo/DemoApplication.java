93d9ee6ecc70ddf30384d68d1d81a5141f056c39
GopalDose <gopaldose12345@gmail.com>
2026-09-12 13:33:47 +0530

    demo port setting and route

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+//		HelloWorldController helloWorldController =new HelloWorldController();
+//
+//		String s = helloWorldController.hello();
+//
+//		System.out.println(s);
+	}
+
+}
